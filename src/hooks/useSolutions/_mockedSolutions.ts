7e819f1a278d82e469ee6055e2aec2342e3a2025
Taha Khaled <tahakhaledabdellah@gmail.com>
2024-02-02 05:15:41 +0200

    add images

--- src/hooks/useSolutions/_mockedSolutions.ts
+++ src/hooks/useSolutions/_mockedSolutions.ts
@@ -3,10 +3,10 @@ export const mockedSolutions = {
   brief:
     " We craft technology solutions that digitally bond and transform the productivity of our customers and their citizens, workers, consumers and partners. ",
   items: [
-    { id: "i1", title: "Transformation", imgUrl: "" },
-    { id: "i2", title: "Envision", imgUrl: "" },
-    { id: "i3", title: "Dynamics 365", imgUrl: "" },
-    { id: "i4", title: "Crafty Mind", imgUrl: "" },
-    { id: "i5", title: "Services", imgUrl: "" },
+    { id: "i1", title: "Transformation", imgUrl: "./Images/1.png" },
+    { id: "i2", title: "Envision", imgUrl: "./Images/2.png" },
+    { id: "i3", title: "Dynamics 365", imgUrl: "./Images/3.png" },
+    { id: "i4", title: "Crafty Mind", imgUrl: "./Images/4.png" },
+    { id: "i5", title: "Services", imgUrl: "./Images/5.png" },
   ],
 };
