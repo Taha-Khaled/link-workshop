e3d189e88aed3b2d68659be5399f547a5e20e684
Taha Khaled <tahakhaledabdellah@gmail.com>
2024-02-02 04:57:30 +0200

    Solutions mocked data

---
+++ src/hooks/useSolutions/_mockedSolutions.ts
@@ -0,0 +1,12 @@
+export const mockedSolutions = {
+  title: " We Deliver Digital Productivity",
+  brief:
+    " We craft technology solutions that digitally bond and transform the productivity of our customers and their citizens, workers, consumers and partners. ",
+  items: [
+    { id: "i1", title: "Transformation", imgUrl: "" },
+    { id: "i2", title: "Envision", imgUrl: "" },
+    { id: "i3", title: "Dynamics 365", imgUrl: "" },
+    { id: "i4", title: "Crafty Mind", imgUrl: "" },
+    { id: "i5", title: "Services", imgUrl: "" },
+  ],
+};
