981b55fccc3839e9f0f201b9ddae22ac1dea3203
Taha Khaled <tahakhaledabdellah@gmail.com>
2024-02-03 05:39:04 +0200

    News List

---
+++ src/components/NewsList/index.tsx
@@ -0,0 +1,37 @@
+import { FunctionComponent, useState } from "react";
+import { NewsEntity } from "../../types/newsType";
+import styles from "./NewsList.module.scss";
+import NewsCard from "../Cards/NewsCard";
+
+interface NewsListProps {
+  news: NewsEntity[];
+}
+
+const NewsList: FunctionComponent<NewsListProps> = ({ news }) => {
+  const [viewAllNews, setViewAllNews] = useState<boolean>(false);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.newsListHolder}>
+        <div className={styles.newsList}>
+          {news?.slice(0, viewAllNews ? news.length : 6)?.map((item) => (
+            <NewsCard key={item.id} {...item} />
+          ))}
+        </div>
+      </div>
+      {news?.length === 0 ? (
+        <h2 className={styles.noNews}>No Related News</h2>
+      ) : (
+        news.length > 6 && (
+          <div className={styles.viewAllNews}>
+            <button onClick={() => setViewAllNews(!viewAllNews)}>
+              {viewAllNews ? "Show Less News" : "View All News"}
+            </button>
+          </div>
+        )
+      )}
+    </div>
+  );
+};
+
+export default NewsList;
